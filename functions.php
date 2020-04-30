3b5beb743aab03576ce827aa12b3364c530b690d
Jay Liu <snowgy920@hotmail.com>
2020-05-01 07:59:49 +0800

    add custom attributes to postmeta & add sort option for attributes

--- functions.php
+++ functions.php
@@ -112,6 +112,7 @@ function woo_custom_out_of_stock() {
     }
 }
 
+// product filter by publication date
 require_once(dirname( __FILE__ ) . '/widgets/date_filter_list.php');
 add_filter( 'posts_clauses', 'pub_date_filter_post_clauses', 11, 2 );
 function pub_date_filter_post_clauses($args, $wp_query ) {
@@ -141,3 +142,192 @@ function pub_date_filter_post_clauses($args, $wp_query ) {
 
     return $args;
 }
+
+
+function porto_save_product_custom_meta($post_id, $post, $update) {
+    $post_type = get_post_type($post_id);
+    // If this isn't a 'product' post, don't update it.
+    if ($post_type != 'product')
+        return;
+
+    if (!empty($_POST['attribute_names']) && !empty($_POST['attribute_values'])) {
+        $attribute_names = $_POST['attribute_names'];
+        $attribute_values = $_POST['attribute_values'];
+        foreach ($attribute_names as $key => $attribute_name) {
+            if ($attribute_name == 'pa_autor') {
+                $custom_author = get_field('author_priority');
+                if (empty($custom_author)) {
+                    $term = get_term_by('id', $attribute_values[$key][0], $attribute_name);
+                    update_post_meta($post_id, $attribute_name, $term->name);
+                } else {
+                    update_post_meta($post_id, $attribute_name, $custom_author[0]->name);
+                }
+            } else {
+                $term = get_term_by('id', $attribute_values[$key][0], $attribute_name);
+                update_post_meta($post_id, $attribute_name, $term->name);
+            }
+        }
+    }
+}
+add_action( 'save_post', 'porto_save_product_custom_meta', 10, 3);
+
+
+// save all attribute info to metainfo
+function porto_update_all_products_metainfo() {
+    $query = new WP_Query( array(
+        'post_type'      => 'product',
+        'post_status'    => 'publish',
+        'posts_per_page' => -1,
+    ) );
+    while ( $query->have_posts() ) {
+        $query->the_post();
+        $post_id = get_the_ID();
+
+        $product = wc_get_product( $post_id );
+        $attributes = $product->get_attributes();
+        foreach ($attributes as $key => $attr) {
+            $options = $attr->get_options();
+            if ($key == 'pa_autor') {
+                $custom_author = get_field('author_priority');
+                if (empty($custom_author)) {
+                    $term = get_term_by('id', $options[0], $key);
+                    update_post_meta($post_id, $key, $term->name);
+                } else {
+                    update_post_meta($post_id, $key, $custom_author[0]->name);
+                }
+            } else {
+                $term = get_term_by('id', $options[0], $key);
+                update_post_meta($post_id, $key, $term->name);
+            }
+        }
+    }
+    exit;
+}
+// add_action('init', 'porto_update_all_products_metainfo');
+
+
+function porto_add_postmeta_ordering_args( $sort_args ) {
+
+	$orderby_value = isset( $_GET['orderby'] ) ? wc_clean( $_GET['orderby'] ) : apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby' ) );
+	switch( $orderby_value ) {
+		case 'publication_date':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'publication_date';
+			break;
+		case 'pa_categorie':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_categorie';
+			break;
+		case 'pa_editura':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_editura';
+			break;
+		case 'pa_autor':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_autor';
+			break;
+		case 'pa_isbn':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_isbn';
+			break;
+		case 'pa_editie':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_editie';
+			break;
+		case 'pa_ziua-aparitiei':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_ziua-aparitiei';
+			break;
+		case 'pa_luna-aparitiei':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_luna-aparitiei';
+			break;
+		case 'pa_anul-aparitiei':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_anul-aparitiei';
+			break;
+		case 'pa_ziua-actualizarii':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_ziua-actualizarii';
+			break;
+		case 'pa_luna-actualizarii':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_luna-actualizarii';
+			break;
+		case 'pa_anul-actualizarii':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_anul-actualizarii';
+			break;
+		case 'pa_format':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_format';
+			break;
+		case 'pa_legare':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_legare';
+			break;
+		case 'pa_coperta':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_coperta';
+			break;
+		case 'pa_hartie':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_hartie';
+			break;
+		case 'pa_numar-pagini':
+			$sort_args['orderby']  = 'meta_value';
+			$sort_args['order']    = 'asc';
+			$sort_args['meta_key'] = 'pa_numar-pagini';
+			break;
+        case 'pa_volum':
+            $sort_args['orderby']  = 'meta_value';
+            $sort_args['order']    = 'asc';
+            $sort_args['meta_key'] = 'pa_volum';
+            break;
+    }
+
+	return $sort_args;
+}
+add_filter( 'woocommerce_get_catalog_ordering_args', 'porto_add_postmeta_ordering_args' );
+
+
+// Add these new sorting arguments to the sortby options on the frontend
+function porto_add_new_postmeta_orderby( $sortby ) {
+	$sortby['publication_date'] = __( 'Sort by publication date', 'woocommerce' );
+	$sortby['pa_categorie'] = __( 'Sort by categorie', 'woocommerce' );
+	$sortby['pa_editura'] = __( 'Sort by editura', 'woocommerce' );
+	$sortby['pa_autor'] = __( 'Sort by autor', 'woocommerce' );
+	$sortby['pa_isbn'] = __( 'Sort by isbn', 'woocommerce' );
+	$sortby['pa_editie'] = __( 'Sort by editie', 'woocommerce' );
+	$sortby['pa_ziua-aparitiei'] = __( 'Sort by ziua apariției', 'woocommerce' );
+	$sortby['pa_luna-aparitiei'] = __( 'Sort by luna apariției', 'woocommerce' );
+	$sortby['pa_anul-aparitiei'] = __( 'Sort by anul apariției', 'woocommerce' );
+	$sortby['pa_ziua-actualizarii'] = __( 'Sort by ziua actualizării', 'woocommerce' );
+	$sortby['pa_luna-actualizarii'] = __( 'Sort by luna actualizării', 'woocommerce' );
+	$sortby['pa_anul-actualizarii'] = __( 'Sort by anul actualizării', 'woocommerce' );
+	$sortby['pa_format'] = __( 'Sort by format', 'woocommerce' );
+	$sortby['pa_legare'] = __( 'Sort by legare', 'woocommerce' );
+	$sortby['pa_coperta'] = __( 'Sort by copertă', 'woocommerce' );
+	$sortby['pa_hartie'] = __( 'Sort by hârtie', 'woocommerce' );
+	$sortby['pa_numar-pagini'] = __( 'Sort by număr pagini', 'woocommerce' );
+	$sortby['pa_volum'] = __( 'Sort by volum', 'woocommerce' );
+	return $sortby;
+}
+add_filter( 'woocommerce_default_catalog_orderby_options', 'porto_add_new_postmeta_orderby' );
+add_filter( 'woocommerce_catalog_orderby', 'porto_add_new_postmeta_orderby' );
